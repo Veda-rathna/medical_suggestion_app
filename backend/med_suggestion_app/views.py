17db60510fbc106e6ae1cfeb54d1655b30bf2b8d
Veda-rathna <vedarathna057@gmail.com>
2025-02-18 19:28:12 +0530

    added chatbot and maps page

--- backend/med_suggestion_app/views.py
+++ backend/med_suggestion_app/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+def chatbot(request):
+    return render(request, 'chatbot.html')
+
+def maps(request):
+    return render(request, 'maps.html')
